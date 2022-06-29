1b5f797e4ec4170944d0bf501467c6bcebd0c212
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-06-29 16:03:55 -0300

    Print de mensagens e comentarios do codigo

---
+++ Aula2/Linhas.py
@@ -0,0 +1,16 @@
+from builtins import print
+
+print('Linha 1')  #comentario linha um
+print('Linha 2')  #Comentario linha dois
+print('Linha 3')  #Comentario linha três
+print('Linha 4')  #Comentario linha Quatro
+print('Linha 5')  #Comentario linha cinco
+print('linha 6')
+""" Utilizado para comentarios grandes
+print('linha 7')
+print('linha 8')"""
+print('linha 9')
+
+
+
+
