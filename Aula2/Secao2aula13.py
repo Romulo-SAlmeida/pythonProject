d30d4fddc4d37fb7afa712de4cbea407d2d33938
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-07-04 12:06:54 -0300

    Utilizando 'end' e 'sep' nos Print

---
+++ Aula2/Secao2aula13.py
@@ -0,0 +1,17 @@
+# ultilizando virgula para dar espaço no print de informações
+print('luis', 'otavio', 'jorge')
+
+#ultilizando varolores no print (Obs.: não precisa ultilizar aspas simples em números
+print(865486)
+
+
+#Ultilizando 'sep' para introduzir "-" na palavra
+print('luis', 'caio', sep = '-')
+
+#utilizando "end" para colocar qualquer coisa no final do print
+print('luis', 'caio', sep = '-', end=' ')
+print(58795)
+
+#fazendo atividades da aula
+print('Atividade')
+print('788.456.123', end= '-12')
