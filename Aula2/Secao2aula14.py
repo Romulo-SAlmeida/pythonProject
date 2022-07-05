4f5125bd152f1765b7173bebe82dce1f1cda3bd4
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-07-05 12:14:16 -0300

    Formatando Strings

---
+++ Aula2/Secao2aula14.py
@@ -0,0 +1,10 @@
+#Utilizado aspas simples e aspas duplas para printar informação na tela do ususario
+print('Essa string é um "texto"')
+print("Essa string é um 'texto'")
+
+#Utilizando barra invertida para printar imformações com simbolos iguais
+print("Essa string é um \"texto\"")
+print('Essa', 'string', 'é', 'um', '\'texto\'', sep = '-')
+
+#Utilizando o 'r' para cancelar qualquer operação dentro do print
+print(r"Essa string é um \n'texto'")
