734604b6110b33db0d1d4a4005ac94abeda9ee34
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-07-06 12:17:28 -0300

    Tipos de dados primitivos

---
+++ Aula2/Secao2aula15.py
@@ -0,0 +1,20 @@
+#Declarando valores e strings e mostrandos seus tipos de dados
+
+#Dados strings 'str'
+print('João', type('João'))
+
+#Dados de números inteiros 'int'
+print(2, type(2))
+
+#Dados de números decimais 'float'
+print(1.25, type(1.25))
+
+#Dados de parametros 'Boolean'
+print(5 ==5 , type(5==5))
+print(6 == 5, type(6 == 5))
+
+#Conversão de dados
+print('10', type('10'), type(int('10')))
+print("1.25", type("1.25"), type(float('1.25')))
+
+
