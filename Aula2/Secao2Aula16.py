4e100cb3e431caae87a79c93e468717488a77af4
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-07-11 12:12:35 -0300

    Operadores ultilizados em python

---
+++ Aula2/Secao2Aula16.py
@@ -0,0 +1,21 @@
+# Operadores aritmeticos
+print('Subtração "-" = ', 10-5)
+print('Mutiplicação "*" = ', 10*5)
+print('Adição "+" = ', 10+5)
+print('Divisão "/" = ', 10/5)
+
+# O operador de multiplicação também é ultilizado para repetição
+print('Repetição com "*" = ', 10 * 'luis ')
+
+#Transformando inteiros em Strings na concatenação
+print('Repetição ' + 'com ' + str(10))
+
+#Divisão de números inteiros com "//"
+print('Divisão de números inteiros 10 e 3 com "//" = ', 10//3)
+
+#Operador ultilizado para potenciação
+print('Potenciação de 2 elevado a 10 com "**" = ', 2**10)
+
+#Operador ultilizado para resto da divisão
+print('Resto da divisão de 10 e 5 = ', 10%5)
+
