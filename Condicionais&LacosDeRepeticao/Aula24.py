34716071c256e3579d12751dac872b1843aa2554
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-12-27 18:57:38 -0300

    TRabalhando com operações

--- Condicionais&LacosDeRepeticao/Aula24.py
+++ Condicionais&LacosDeRepeticao/Aula24.py
@@ -1,13 +1,13 @@
 """"
 Trabalhando com comparações
 """
-* comparador de igualdade
+# comparador de igualdade
 a = 2
 b = 3
 comparar = (a == b)
 print(comparar)
 
-comparador de maior que >
+# comparador de maior que >
 a = 2
 b = 3
 
