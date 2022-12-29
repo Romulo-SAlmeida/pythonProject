b83734fa48f0aa76b9b60679ecd11cbf5fcad716
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-12-28 21:26:39 -0300

    Ultilizando condicionais IF e ELSE

---
+++ Condicionais&LacosDeRepeticao/Aula34.py
@@ -0,0 +1,12 @@
+"""
+Ultilizando condicionais 
+"""
+entrada = input('Você deseja sair do sistema?')
+
+if entrada == 'sim':
+    print('Saida com sucesso')
+elif entrada == 'não':
+    print('Ok, continuar no sistema')
+else:
+    print('Erro')
+
