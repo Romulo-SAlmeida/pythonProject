a68e85f892462ead4845ba256a090fe939e850f5
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-12-28 21:31:03 -0300

    Ultilizando condicionais

--- Condicionais&LacosDeRepeticao/Aula34.py
+++ Condicionais&LacosDeRepeticao/Aula34.py
@@ -3,7 +3,7 @@ Ultilizando condicionais
 """
 entrada = input('Você deseja sair do sistema?')
 
-if entrada == 'sim':
+if entrada == 'sim':                          #Após construir a condicional e ir para próxima linha sempre dar TAB 
     print('Saida com sucesso')
 elif entrada == 'não':
     print('Ok, continuar no sistema')
