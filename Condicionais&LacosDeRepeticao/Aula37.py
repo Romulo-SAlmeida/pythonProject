29a9c1f87d02effa9e9a777c9934441d1f1b00bb
Romulo-SAlmeida <raaalmeida9@gmail.com>
2022-12-29 13:01:23 -0300

    Trabalhando com operadores de comparação

---
+++ Condicionais&LacosDeRepeticao/Aula37.py
@@ -0,0 +1,11 @@
+"""
+Trabalhando com operadores de comparação
+"""
+
+maior = 2>1   #Coparação que especifica que 2 é maior que 1
+menor = 0<1  #Coparação que especifica que 0 é menor que 1
+igualOuMaior = 2>=1 #Comparação que especifica que 2 e igual ou maior que 1
+igualOUMenor = 0<=1 #Comparação que especifica que 0 é igual ou menor que 1
+igual = 2 == 2 #Comparação que diz ue o primeiro valor é igual o segundo
+diferente = 2 != 1  #Comparação que especifica valores diferentes
+e = 2 
