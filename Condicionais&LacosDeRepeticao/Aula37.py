cc454423604e73fac7bd5332c5d1ad03ed4151aa
Romulo-SAlmeida <raaalmeida9@gmail.com>
2023-01-05 23:39:43 -0300

    Ultilizando operadaores para laços de condicionais

--- Condicionais&LacosDeRepeticao/Aula37.py
+++ Condicionais&LacosDeRepeticao/Aula37.py
@@ -8,4 +8,25 @@ igualOuMaior = 2>=1 #Comparação que especifica que 2 e igual ou maior que 1
 igualOUMenor = 0<=1 #Comparação que especifica que 0 é igual ou menor que 1
 igual = 2 == 2 #Comparação que diz ue o primeiro valor é igual o segundo
 diferente = 2 != 1  #Comparação que especifica valores diferentes
-e = 2 
+
+
+entrada_Cpf = input('Digite seu CPF ')      #Comparação ultilizando 'and' para acrescentar mais uma opção no laço condicional
+entrada_senha = input('Digite sua senha ')               
+if entrada_senha == '123456789' and entrada_Cpf == '70713877103':
+    input('Acesso permitido')
+else:
+    input('Acesso negado')
+
+
+
+busca_placa = input('Nome do dispositivo:')            #Ultilize o 'or' para criar um 'ou'
+placa = 'XVCMMega'                         
+if busca_placa == placa or busca_placa == 'xvcmmega':
+    print("Encontrado")
+else:
+    print('Não encontrado')
+
+
+
+
+
