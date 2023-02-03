ce17a99a9941f80f1516120fcca22b479a53118d
Romulo-SAlmeida <raaalmeida9@gmail.com>
2023-02-02 23:26:41 -0300

    Fatiamento de Strings e exercicio python

---
+++ Condicionais&LacosDeRepeticao/Aula43.py
@@ -0,0 +1,24 @@
+"""
+Fatiamento de Strings e função len
+"""
+
+variavel = 'Romulo'
+print(variavel[0:2])           # Selecionando o indice da variavel
+print(len(variavel))           #Função 'len' para contar a quantidades de caracteres
+
+#Exercicio aula 47
+
+
+print('-------------------------------')
+nome = input('  Digite o seu nome  ')
+
+print('-------------------------------')
+
+idade = int(input('  Digite a sua idade  '))
+
+if idade>=0 and nome != 0:
+    print('Junior')
+
+
+
+
