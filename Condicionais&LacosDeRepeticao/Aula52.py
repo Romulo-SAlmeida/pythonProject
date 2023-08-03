627ccf1adab9339c96deb2a65c733ae5fad1e75e
Romulo-SAlmeida <raaalmeida9@gmail.com>
2023-08-02 22:21:21 -0300

    Prendando sobre 'id'

---
+++ Condicionais&LacosDeRepeticao/Aula52.py
@@ -0,0 +1,14 @@
+"""
+Ultilizando 'id' 
+
+Flag (Bandeira) - marca um local
+None = Não valor
+is e is not = é ou não é
+id = identidade(Aonde a variavel fica na memoria)
+"""
+
+v1 = 'a'
+v2 = 'b'
+
+print(id(v2))
+
