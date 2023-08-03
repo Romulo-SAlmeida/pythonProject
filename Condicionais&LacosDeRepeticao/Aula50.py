7653a38c35c7c717da5e1783ebbf7be24c03d94a
Romulo-SAlmeida <raaalmeida9@gmail.com>
2023-08-02 22:03:48 -0300

    Dicas para melhorar o codigo

---
+++ Condicionais&LacosDeRepeticao/Aula50.py
@@ -0,0 +1,16 @@
+"""Deixando os codígos mais limpos e legiveis para um programador"""
+
+# Constante = VAriaveis que não vão mudar de valor, serão em letra maiuscula
+# Muitas condições no mesmo if é ruim
+# Muito espaço no codigo gera complexidade e complexidade não é bom
+# O simples as vezes é o melhor
+# Sempre deixe os codigos mais limpos e faceis de entender
+ 
+
+velocidade = 60 #velociade atual do carro
+local_carro = 100 # local em que carro está no estrada
+
+RADAR_1= 60 #Velociade máxima do radar 1
+LOCAL_1= 100 # local onde o radar está
+RADAR_RANGE : 1 # A distancia onde o radar pega
+
