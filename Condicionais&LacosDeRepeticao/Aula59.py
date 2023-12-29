1ffd36d9682b4afb57a4f0b3cc07e7099bbe895d
Romulo-SAlmeida <raaalmeida9@gmail.com>
2023-12-28 22:15:45 -0300

    Ultilizando While

---
+++ Condicionais&LacosDeRepeticao/Aula59.py
@@ -0,0 +1,22 @@
+"""  Laço de repetição 'While' """
+
+"""
+senha = int(input('Digite a sua senha: ')) 
+while senha != 123:
+    print('Acesso negado')
+    print('Tente novamente')
+    senha = int(input('Digite a sua senha: '))   
+    if senha == 123:
+        print("Acesso permitido")
+"""
+"""
+contador = 0
+while contador <= 10:
+    contador += 1
+    print(contador)
+    if contador == 8:   
+        break
+"""
+
+
+    
