1d48f1422cdc81665c5be588b7955d586b395d72
Romulo-SAlmeida <raaalmeida9@gmail.com>
2024-01-09 23:14:32 -0300

    ...

---
+++ EstruturaSequencial/Atividade3.py
@@ -0,0 +1,9 @@
+"""Faça um Programa que peça dois números e imprima a soma."""
+print('----------------------------------------------------------')
+numero1 = int(input('Digite o primeiro valor: '))
+numero2 = int(input('Digite o segundo valor: '))
+
+soma_number = numero1+numero2
+
+print("O resultado da soma dos dois valores é ", soma_number)
+print('----------------------------------------------------------')
