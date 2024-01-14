d817253e7548ddb73eeaeb7803505b53ee9e098a
Romulo-SAlmeida <raaalmeida9@gmail.com>
2024-01-14 12:40:46 -0300

    '#'

---
+++ EstruturaSequencial/Atividade4.py
@@ -0,0 +1,11 @@
+"""
+Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""
+print("---------------------------------------------------------------")
+nota_n1 = float(input("\nDigite a nota do primeiro bimestre: "))
+nota_n2 = float(input("\nDigite a nota do segundo bimestre: "))
+nota_n3 = float(input("\nDigite a nota do terceiro bimestre: "))
+nota_n4 = float(input("\nDigite a nota do quarto bimestre: "))
+nota_final = (nota_n1+nota_n2+nota_n3+nota_n4)/4
+print("\nSua nota final é ", nota_final )
+print("---------------------------------------------------------------")
