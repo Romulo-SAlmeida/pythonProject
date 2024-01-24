91d281e2132681a82a2c2e912e5ae1a71e57a5ac
Romulo-SAlmeida <raaalmeida9@gmail.com>
2024-01-23 22:11:03 -0300

    ...

---
+++ EstruturaSequencial/Atividade6.py
@@ -0,0 +1,6 @@
+""" Faça um Programa que peça o raio de um círculo, calcule e mostre sua área """
+
+raio = float(input("Qual é o raio desse circulo: "))
+area_circulo = 3.14*(raio*raio)
+
+print(f"A areá do seu circulo é {area_circulo}")
---
+++ EstruturaSequencial/Atividade7.py
@@ -0,0 +1,6 @@
+""" Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. """
+
+lado = float(input("Digite o valor de um dos lados do quadrado "))
+area = lado*lado
+
+print(f"A aréa deste quadrado é {area} e seu dobro é {area*area}")
